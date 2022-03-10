4cf9c40afe77c086155131a6b4a17a69c50ad817
Anders Sandström <anders.sandstrom@esss.se>
2022-03-10 11:38:48 +0100

    torque velo

--- rotor_position/calcs/plot12hTorqeVelo.bash
+++ rotor_position/calcs/plot12hTorqeVelo.bash
@@ -1,2 +1,2 @@
-cat 12hdata/12h_test_*.log ../../doc/torque_time.log | grep "EL5021\|Torque" | awk '{print $1 " " $2 " " $3 " " $4}'| bash calcVeloCa.bash | grep "Velo\|Torque" | bash downSampleMaxMin.bash Velo 100 | bash filterOutliers.bash Velo 1 | python ~/sources/ecmccomgui/pyDataManip/plotCaMonitor.py 
+cat 12hdata/12h_test_*.log ../../doc/torque_time.log | grep "EL5021\|Torque" | awk '{print $1 " " $2 " " $3 " " $4}'| bash calcVeloCa.bash | grep "Velo\|Torque" | bash downSampleMaxMin.bash Velo 100 | bash filterOutliers.bash Velo 0.2 | bash gain.bash Torque 5.8 |python plot12HTorqueVelo.py
 
